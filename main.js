fc288b5323f58d893e315832789797b878feec92
Shubh Shukla <shubh.shukla2001@gmail>
2022-10-12 15:19:30 +0530

    updated the chori files

--- main.js
+++ main.js
@@ -24515,5 +24515,15 @@ Webflow.require('ix').init([
 
 
 
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
 
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var menu = document.querySelector('.menu');
+  menu.classList.toggle('active')
+  menuToggle.classList.toggle('active');
+}
   
