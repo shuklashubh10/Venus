d701d47feb415f0dcf14a5e1d813bf1bd1cc5364
Shubh Shukla <shubh.shukla2001@gmail>
2022-11-11 18:30:11 +0530

    fjkasld

--- main.js
+++ main.js
@@ -24546,5 +24546,15 @@ toggleIcon.addEventListener('click', () => {
     }
 });
 
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
 
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var menu = document.querySelector('.menu');
+  menu.classList.toggle('active')
+  menuToggle.classList.toggle('active');
+}
 
