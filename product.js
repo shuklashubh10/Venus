516926eac8e951970b0eccd7145b9ad13078cbe0
Saiyam-19 <saiyamchaplot19@gmail.com>
2022-11-30 19:42:25 +0530

    Almost complete

---
+++ product.js
@@ -0,0 +1,77 @@
+function changeStuff1(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[0], null).display == "none") {
+        document.getElementsByClassName("textSection")[0].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[0].style.display = "none";
+    }
+}
+function changeStuff2(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[1], null).display == "none") {
+        document.getElementsByClassName("textSection")[1].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[1].style.display = "none";
+    }
+}
+function changeStuff3(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[2], null).display == "none") {
+        document.getElementsByClassName("textSection")[2].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[2].style.display = "none";
+    }
+}
+function changeStuff4(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[3], null).display == "none") {
+        document.getElementsByClassName("textSection")[3].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[3].style.display = "none";
+    }
+}
+function changeStuff5(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[4], null).display == "none") {
+        document.getElementsByClassName("textSection")[4].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[4].style.display = "none";
+    }
+}
+function changeStuff6(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[5], null).display == "none") {
+        document.getElementsByClassName("textSection")[5].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[5].style.display = "none";
+    }
+}
+function changeStuff7(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[6], null).display == "none") {
+        document.getElementsByClassName("textSection")[6].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[6].style.display = "none";
+    }
+}
+function changeStuff8(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[7], null).display == "none") {
+        document.getElementsByClassName("textSection")[7].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[7].style.display = "none";
+    }
+}
+function changeStuff9(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[8], null).display == "none") {
+        document.getElementsByClassName("textSection")[8].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[8].style.display = "none";
+    }
+}
+function changeStuff10(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[9], null).display == "none") {
+        document.getElementsByClassName("textSection")[9].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[9].style.display = "none";
+    }
+}
+function changeStuf11(event) {
+    if(getComputedStyle(document.getElementsByClassName("textSection")[10], null).display == "none") {
+        document.getElementsByClassName("textSection")[10].style.display = "block"
+    } else {
+        document.getElementsByClassName("textSection")[10].style.display = "none";
+    }
+}
