9470bd50d2750e1919e6949b2765c2393d0ba802
Shubh Shukla <shubh.shukla2001@gmail>
2023-02-01 12:59:08 +0530

    added the preloader

--- main.js
+++ main.js
@@ -24558,3 +24558,17 @@ function toggleMenu() {
   menuToggle.classList.toggle('active');
 }
 
+
+$.get("preloader.html", function(data){
+  $("#loading").replaceWith(data);
+});
+
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( ".loader-wrapper" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( ".loader-wrapper" ).remove(); //makes page more lightweight 
+  });   
+}
